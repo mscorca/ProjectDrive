ab5021c07fb84eae0792e0a119ad54b415670eae
Matt Scorca <mscorca@ucsc.edu>
2015-04-02 12:40:48 -0700

    Initial project

---
+++ Assets/RainController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RainController : MonoBehaviour {
+
+	public GameObject player;
+	private Transform t;
+	private Transform otherT;
+	private ParticleSystem ps;
+	
+	public float activateDist = 200f;
+	private float dist;
+
+	void Start () {
+		t = GetComponent<Transform>();
+		ps = GetComponent<ParticleSystem>();
+		otherT = player.transform;
+	}
+	
+	void Update () {
+		dist = Vector3.Distance(t.position, otherT.position);
+
+		if(dist <= activateDist && !ps.isPlaying){
+			ps.Play();
+		} else if (dist >= activateDist) {
+			ps.Stop();
+		}
+	}
+}
---
+++ Assets/Scripts/RoadController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RoadController : MonoBehaviour {
+
+	public Transform[] roads;
+	public int roadCount;
+	private Transform t;
+
+	// Use this for initialization
+	void Start () {
+		t = GetComponent<Transform>();
+
+		roads = gameObject.GetComponentsInChildren<Transform>();
+		Transform prevRoad = roads[0];
+		Debug.Log(prevRoad.position);
+
+
+		foreach(Transform road in roads){
+			if(road != roads[0]){
+				Debug.Log("Road i: " + road.position.x + " " + prevRoad.position.z);
+				road.position = (new Vector3(0, 0.1f, prevRoad.position.z + 16.0f));
+				prevRoad = road;
+			}
+		}
+	}
+}
